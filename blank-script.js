735a12eb3b494064c0a819815d2cba1e9c915c11
Taehong888 <kthg8060@naver.com>
2025-06-05 22:45:20 +0900

    Update blank-script.js

--- blank-script.js
+++ blank-script.js
@@ -194,7 +194,7 @@ function createLabelAndCheckbox() {
 
   const resultDiv = document.createElement('div');
   resultDiv.style.backgroundColor = '#b8fcb8';
-  resultDiv.style.padding = ''6px' '20px';
+  resultDiv.style.padding = '6px' '20px';
   resultDiv.style.borderRadius = '5px';
   resultDiv.style.marginBottom = '10px';
   resultDiv.append(checkbox, label, controlArea);
